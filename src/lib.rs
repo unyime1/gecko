09e44cdd44cd4b6912afc6cc9f9d2f90348f2b92
unyime1 <lordunyime@gmail.com>
2022-12-10 19:43:50 +0100

    setup open operation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,38 @@
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::hash::Hash;
+use std::io;
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter, SeekFrom};
+use std::path::Path;
 
+use serde_derive::{Deserialize, Serialize};
+
+type ByteString = Vec<u8>;
+type ByteStr = [u8];
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyValuePair {
+    pub key: ByteString,
+    pub value: ByteString,
+}
+
+#[derive(Debug)]
+pub struct GeckoKV {
+    f: File,
+    pub index: HashMap<ByteString, u64>,
+}
+
+impl GeckoKV {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        let f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(path)?;
+        
+            let index = HashMap::new();
+            Ok(Self{f, index})
+    }
+}
